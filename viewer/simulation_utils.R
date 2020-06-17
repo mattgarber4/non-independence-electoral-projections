1ff521662ede9a66945c7c28aeb4e21e9579d8cf
mattgarber4 <48842179+mattgarber4@users.noreply.github.com>
2020-06-17 13:12:20 -0400

    Add files via upload

--- viewer/simulation_utils.R
+++ viewer/simulation_utils.R
@@ -1,9 +1,9 @@
 dependenceCoefs <- c(0, 
-                     seq(0.0001, 0.001, 0.0001), 
-                     seq(0.0015, 0.01, 0.0005), 
+                     seq(0.0001, 0.001, 0.0001),
+                     seq(0.0015, 0.01, 0.0005),
                      seq(0.015, 0.045, 0.005),
                      seq(.05, .1, .01),
-                     seq(0.2, 1, 0.1), 
+                     seq(0.2, 1, 0.1),
                      seq(2, 4, 1))
 shifts <- c(-1, -.5, -.25, -.1, -.075, seq(-.05, .05, .01), .075, .1, .25, .5, 1)
 
@@ -61,7 +61,15 @@ electionSim <- setRefClass("electionSim",
                                    
                                },
                                
-                               plot = function(sub = NULL) {
+                               plot = function(sub = NULL, main = TRUE) {
+                                   if (main == TRUE) {
+                                       m <- "Simulated Elections"
+                                   } else if (main == FALSE) {
+                                       m <- NULL
+                                   } else {
+                                       m <- main
+                                   }
+                                   par(lwd = .1)
                                    hist(
                                        elections,
                                        breaks = c(0, seq(4, 268, 8), seq(270, 538, 8), 538),
@@ -70,12 +78,66 @@ electionSim <- setRefClass("electionSim",
                                        xlim = c(0, 538),
                                        xlab = "Democratic Electoral Votes",
                                        ylab = "",
-                                       main = "Simulated Elections",
+                                       main = m,
                                        sub = sub
                                    )
                                    
-                                   abline(v = 270)
+                                   text(x = c(0, 269, 538),
+                                        y = 0,
+                                        pos = 1,
+                                        xpd = T,
+                                        labels = c("0", "269", "538"))
+                                   segments(x0 = 269, y0 = 0, y1 = 10)
                                    
+                                   
+                               },
+                               
+                               plotMeans = function(main = NULL) {
+                                   idx <- order(sim$stateMeans)
+                                   len <- length(sim$stateMeans)
+                                   barplot(rbind(sim$stateMeans[idx],1 - sim$stateMeans[idx]),
+                                           ylim = c(0, 1),
+                                           col = c(demBlue, gopRed),
+                                           width = 1,
+                                           space = c(0, rep(.1, length(sim$stateMeans) - 1)),
+                                           las = 2,
+                                           axes = F,
+                                           border = F,
+                                           main = main
+                                   )
+                                   segments(
+                                       x0 = 0,
+                                       x1 = len * 1.1 - .1,
+                                       y0 = sim$winPct)
+                                   text(len * 1.1 + .75,
+                                        sim$winPct,
+                                        paste0(round(100 * sim$winPct, 1), "%"), 
+                                        pos = 4, 
+                                        xpd = T)
+                                   Arrowhead(x0 = len * 1.1 + .2, 
+                                                    y0 = sim$winPct, 
+                                                    arr.adj = 1, 
+                                                    angle = 180,
+                                                    arr.type = 'triangle',
+                                                    xpd = T)
                                }
                            ))
 electionSim$lock(names(electionSim$fields()))
+
+# State label function
+translateToState <- function(states) {
+    out <- state.name[match(states, state.abb)]
+    out[states == "DC"] <- "district of columbia"
+    tolower(out)
+}
+
+# For maps - gives color on line in 3-space between demBlue and gopRed. Parameter t = 1 gives demBlue, t = 0 gives gopRed
+linearParam <- function(t) {
+    val <- sapply(t, function(m) col2rgb(demBlue) + (1 - m) * (col2rgb(gopRed) - col2rgb(demBlue)))
+    rgb(val[1,], val[2, ], val[3, ], maxColorValue = 255)
+}
+
+# Maps dem percent along split graph - more blue for about 50%, more red for below
+colorMapper <- function(demPct) {
+    ifelse(demPct < .5, linearParam(3 * demPct / 4), linearParam(5 / 8 + 3 * (demPct - .5) / 4))
+}
